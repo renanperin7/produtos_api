38c1b120477654c542e3503d3cbb48b9e705965e
Renan <renanperin27@hotmail.com>
2025-02-10 18:06:02 -0300

    conclusao secao 2 do curso

--- src/main/java/io/renanperin/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/io/renanperin/produtosapi/repository/ProdutoRepository.java
@@ -2,6 +2,9 @@ package io.renanperin.produtosapi.repository;
 
 import io.renanperin.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
 }
